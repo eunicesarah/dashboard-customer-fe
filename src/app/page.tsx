85ee1430962023557a57f027a3f4e7d4a39dee1d
Bagusbimawan <bagusbimawansembodo@gmail.com>
2024-10-10 20:42:46 +0700

    stye:fix style in dashboard and some remove

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,17 @@
-import ECommerce from "@/components/Dashboard/E-commerce";
 import { Metadata } from "next";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
+import BasicChartPage from "./chart/page";
 
 export const metadata: Metadata = {
-  title:
-    "Next.js E-commerce Dashboard | TailAdmin - Next.js Dashboard Template",
-  description: "This is Next.js Home for TailAdmin Dashboard Template",
+  title: "Dashboard Customer",
+  description: "Dashboard Customer",
 };
 
 export default function Home() {
   return (
     <>
       <DefaultLayout>
-        <ECommerce />
+        <BasicChartPage />
       </DefaultLayout>
     </>
   );
--- src/components/Charts/page.tsx
+++ src/components/Charts/page.tsx
@@ -1,6 +1,4 @@
 "use client";
-import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
-import ChartOne from "@/components/Charts/ChartOne";
 import ChartTwo from "@/components/Charts/ChartTwo";
 import ChartFour from "@/components/Charts/ChartFour";
 import dynamic from "next/dynamic";
@@ -13,14 +11,13 @@ const ChartThree = dynamic(() => import("@/components/Charts/ChartThree"), {
 const Chart: React.FC = () => {
   return (
     <>
-      <Breadcrumb pageName="Chart" />
-
       <div className="grid grid-cols-12 gap-4 md:gap-6 2xl:gap-7.5">
-        <ChartOne />
         <ChartTwo />
         <ChartThree />
       </div>
-        <ChartFour/>
+      <div className="mt-5">
+        <ChartFour />
+      </div>
     </>
   );
 };
